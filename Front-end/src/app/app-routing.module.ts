78420cdb187ac7b934f656a71c33cbab9bb0f74d
Lucas Pitt de Lima <lucaspitt@hotmail.com>
2018-09-05 15:50:50 -0300

    Implementação da tela de relatorio, e funções onclick

--- Front-end/src/app/app-routing.module.ts
+++ Front-end/src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { RouterModule, Routes } from '@angular/router';
 // Components
 import { LoginComponent } from './login/login.component';
 import { AdminDashboardComponent } from './admin/admindashboard/admindashboard.component';
+import { RelatorioComponent } from './relatorio/relatorio.component';
 
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
-  { path: 'admin', component: AdminDashboardComponent }
+  { path: 'admin', component: AdminDashboardComponent },
+  { path: 'relatorio', component: RelatorioComponent }
 
 ];
 
