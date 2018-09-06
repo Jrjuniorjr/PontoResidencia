7befb1383dc36251f29ec7618ee28bf25f3ac2c1
Euller Nóbrega <eullernobrega@gmail.com>
2018-09-06 11:46:21 -0300

    Correcao Controller Professor

--- springboot-baterPonto/src/main/java/br/com/unicap/springboot/springbootbaterPonto/controller/ControllerProfessor.java
+++ springboot-baterPonto/src/main/java/br/com/unicap/springboot/springbootbaterPonto/controller/ControllerProfessor.java
@@ -13,11 +13,17 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  *
  * @author Euller
  */
+
+
+@RestController
+@RequestMapping("/professor")
 public class ControllerProfessor {
     @Autowired
     DAOProfessor c;
