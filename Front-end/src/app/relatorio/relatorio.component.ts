1e1bcb65599c73e563e3ab3e20f570c10cdbf473
Lucas Pitt de Lima <lucaspitt@hotmail.com>
2018-09-06 10:07:47 -0300

    Alteraçoes no filtro de mes e aluno da tela relatorio

--- Front-end/src/app/relatorio/relatorio.component.ts
+++ Front-end/src/app/relatorio/relatorio.component.ts
@@ -1,12 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import {Relatorio} from 'C:/Users/Lucas/Desktop/Faculdade/Residencia/teste/src/app/Relatorio';
 import {Router} from '@angular/router'
+import {Aluno} from 'C:/Users/Lucas/Desktop/Faculdade/Residencia/teste/src/app/aluno'
+
+enum Meses {
+  Janeiro = 1,
+  Fevereiro,
+  Março,
+  Abril,
+  Maio,
+  Junho,
+  Julho,
+  Agosto,
+  Setembro,
+  Outubro,
+  Novembro,
+  Dezembro
+};
 
 @Component({
   selector: 'app-relatorio',
   templateUrl: './relatorio.component.html',
   styleUrls: ['./relatorio.component.css']
 })
+
 export class RelatorioComponent implements OnInit {
 
   constructor(public router:Router) { }
@@ -14,6 +31,14 @@ export class RelatorioComponent implements OnInit {
   ngOnInit() {
   }
 
+  meses:string [] = [
+      'Janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'
+  ]
+
+  //public enum Mes {J,k,t};
+
+
+  
 relatorios : Relatorio[] = [
 
   {nome:'Glauber',matricula:'1241241',somatorioH:50,entrada:1420,saida:1800},
@@ -24,10 +49,17 @@ relatorios : Relatorio[] = [
 
 ]
 
+alunos : Aluno[] = [
+
+  {nome:'Glauber',matricula:'1241241',id:1,login:'2929',password:'x'},
+]
+
 voltarTelaAdmin() {
 
   this.router.navigate(["/admin"])
 
 }
+
+
 
 }
