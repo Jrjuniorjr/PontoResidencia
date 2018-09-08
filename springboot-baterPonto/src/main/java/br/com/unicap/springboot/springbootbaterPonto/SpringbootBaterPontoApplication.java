53432f30cbbd959862a11214eab17448a3f2741e
Jean <jean_luccas320@hotmail.com>
2018-09-08 02:23:37 +0000

    feito o override de run, mas foi removido

--- springboot-baterPonto/src/main/java/br/com/unicap/springboot/springbootbaterPonto/SpringbootBaterPontoApplication.java
+++ springboot-baterPonto/src/main/java/br/com/unicap/springboot/springbootbaterPonto/SpringbootBaterPontoApplication.java
@@ -1,5 +1,5 @@
 package br.com.unicap.springboot.springbootbaterPonto;
-
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,11 +7,12 @@ import br.com.unicap.springboot.springbootbaterPonto.model.Aluno;
 import br.com.unicap.springboot.springbootbaterPonto.repository.AlunoRepository;
 
 @SpringBootApplication
-public class SpringbootBaterPontoApplication {
+public class SpringbootBaterPontoApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBaterPontoApplication.class, args);
 		
 		
 	}
+	
 }
