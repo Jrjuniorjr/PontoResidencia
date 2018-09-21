fef73d1f5317e28ebfc63c30fb7b45611e489259
Avelino Gomez Alonso Jr <avelino.gomez.jr@hotmail.com>
2018-09-21 12:40:30 -0300

    Deixando codigo bonitinho

--- springboot-baterPonto/src/main/java/br/com/unicap/springboot/springbootbaterPonto/repository/RelatorioRepository.java
+++ springboot-baterPonto/src/main/java/br/com/unicap/springboot/springbootbaterPonto/repository/RelatorioRepository.java
@@ -7,6 +7,7 @@ import javax.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import br.com.unicap.springboot.springbootbaterPonto.model.Relatorio;
 import br.com.unicap.springboot.springbootbaterPonto.model.RelatorioKey;
@@ -14,32 +15,32 @@ import br.com.unicap.springboot.springbootbaterPonto.model.RelatorioKey;
 public interface RelatorioRepository extends JpaRepository<Relatorio, RelatorioKey>{
 	
 	//
-	@Query(value = "SELECT * FROM tbl_relatorio WHERE matricula = ?1",nativeQuery = true)
-    public ArrayList<Relatorio> listarRelatoriosByMatricula(String matricula);
+	@Query(value = "SELECT * FROM tbl_relatorio WHERE matricula like :matricula",nativeQuery = true)
+    public ArrayList<Relatorio> listarRelatoriosByMatricula(@Param("matricula") String matricula);
 	
 	@Query(value = "SELECT * FROM tbl_relatorio",nativeQuery = true)
 	public ArrayList<Relatorio> listarRelatorios();
 	
-	@Query(value = "SELECT * FROM tbl_relatorio WHERE (matricula = ?1 and data = DATE(sysdate()))",nativeQuery = true)
-    public Relatorio listarRelatorioHoje(String matricula);
+	@Query(value = "SELECT * FROM tbl_relatorio WHERE (matricula like :matricula and data = DATE(sysdate()))",nativeQuery = true)
+    public Relatorio listarRelatorioHoje(@Param("matricula") String matricula);
 	
 	//Bater ponto
 	@Modifying
-	@Query(value = "INSERT INTO tbl_relatorio (matricula,data,entrada) values ( ?1,DATE(sysdate()), TIME(sysdate()))",nativeQuery = true)
+	@Query(value = "INSERT INTO tbl_relatorio (matricula,data,entrada) values (:matricula ,DATE(sysdate()), TIME(sysdate()))",nativeQuery = true)
 	@Transactional
-	public void baterPontoEntrada(String matricula);
+	public void baterPontoEntrada(@Param("matricula") String matricula);
 	
 	@Modifying
-	@Query(value = "UPDATE tbl_relatorio SET saida=sysdate() WHERE (matricula = ?1 and DATE(sysdate())) = data",nativeQuery = true)
+	@Query(value = "UPDATE tbl_relatorio SET saida=sysdate() WHERE (matricula like :matricula and DATE(sysdate())) = data",nativeQuery = true)
 	@Transactional
-	public void baterPontoSaida(String matricula);
+	public void baterPontoSaida(@Param("matricula") String matricula);
 	
 	//Verificação se já bateu ponto
-	@Query(value = "select count(*) from tbl_relatorio where (matricula = ?1 and data=DATE(sysdate()))",nativeQuery = true)
-	public int getEntrarHoje(String matricula);
+	@Query(value = "select count(*) from tbl_relatorio where (matricula like :matricula and data=DATE(sysdate()))",nativeQuery = true)
+	public int getEntrarHoje(@Param("matricula") String matricula);
 	
-	@Query(value = "select count(*) from tbl_relatorio where (matricula = ?1 and data=DATE(sysdate()) and entrada != null and saida = null)",nativeQuery = true)
-	public int getSairHoje(String matricula);
+	@Query(value = "select count(*) from tbl_relatorio where (matricula like :matricula and data=DATE(sysdate()) and entrada != null and saida = null)",nativeQuery = true)
+	public int getSairHoje(@Param("matricula") String matricula);
 	
 	
 	
--- springboot-baterPonto/src/main/java/br/com/unicap/springboot/springbootbaterPonto/repository/ResidenteRepository.java
+++ springboot-baterPonto/src/main/java/br/com/unicap/springboot/springbootbaterPonto/repository/ResidenteRepository.java
@@ -16,15 +16,15 @@ public interface ResidenteRepository extends JpaRepository<Residente, String>{
 	@Query(value = "SELECT * from tbl_residente where token like :token",nativeQuery = true)
 	public Residente getResidenteByToken(@Param("token") String token);
 
-	@Query(value = "SELECT * from tbl_residente where matricula = ?1",nativeQuery = true)
-	public Residente getResidenteByMatricula(String matricula);
+	@Query(value = "SELECT * from tbl_residente where matricula like :matricula",nativeQuery = true)
+	public Residente getResidenteByMatricula(@Param("matricula") String matricula);
 	
 	@Query(value = "SELECT * from tbl_residente where tipo=0",nativeQuery = true)
 	public List<Residente> getResidentes();
 	
 	@Modifying
-	@Query(value = "UPDATE tbl_residente SET token=?1 WHERE matricula = ?2",nativeQuery = true)
+	@Query(value = "UPDATE tbl_residente SET token like :token WHERE matricula like :matricula",nativeQuery = true)
 	@Transactional
-	public void atualizarToken(String token, String matricula);
+	public void atualizarToken(@Param("token")String token, @Param("matricula")String matricula);
 	
 }
